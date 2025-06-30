12ded30d64b7f81437cec1f7c4b12a2a7074f1a1
Sal7ySalt <danielzhang309@yahoo.com>
2025-06-30 11:47:04 +0200

    testing decoder functions

--- Steane/steane_ec_decoder.py
+++ Steane/steane_ec_decoder.py
@@ -22,6 +22,36 @@ tab2_z = [{"syndrome": [1, -1, 1], "correction": "IIXIIIX"},
 tab2_x = [{"syndrome": [1, -1, 1], "correction": "IIZIIIZ"},
           {"syndrome": [1, 1, -1], "correction": "IIIZIZI"}]
 
+def find_no_flag_syndrome(no_flag_allqubits_z, no_flag_allqubits_x):
+    parity1_z = 1
+    for i in [0, 2, 4, 6]:
+        parity1_z *= no_flag_allqubits_z[i]
+
+    parity2_z = 1
+    for i in [3, 4, 5, 6]:
+        parity2_z *= no_flag_allqubits_z[i]
+        
+    parity3_z = 1
+    for i in [1, 2, 5, 6]:
+        parity3_z *= no_flag_allqubits_z[i]
+        
+    parity1_x = 1
+    for i in [0, 2, 4, 6]:
+        parity1_x *= no_flag_allqubits_x[i]
+
+    parity2_x = 1
+    for i in [3, 4, 5, 6]:
+        parity2_x *= no_flag_allqubits_x[i]
+        
+    parity3_x = 1
+    for i in [1, 2, 5, 6]:
+        parity3_x *= no_flag_allqubits_x[i]
+        
+    syndrome_no_flag_z = [parity1_z, parity2_z, parity3_z]
+    syndrome_no_flag_x = [parity1_x, parity2_x, parity3_x]
+    
+    return syndrome_no_flag_z, syndrome_no_flag_x
+
 def bool_syndrome_flag(syndrome_flag_z, syndrome_flag_x):
     if (syndrome_flag_z == [1, 1, 1] and syndrome_flag_x == [1, 1, 1]):
         return True
