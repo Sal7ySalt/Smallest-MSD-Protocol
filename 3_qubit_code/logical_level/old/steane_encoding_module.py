89974cb4507d6ebbaabe5fda679356d43f6b945b
Sal7ySalt <danielzhang309@yahoo.com>
2026-05-25 22:18:22 -0400

    Reorganized folders and files

---
+++ 3_qubit_code/logical_level/old/steane_encoding_module.py
@@ -0,0 +1,306 @@
+# Welcome to the steane encoding module! 
+
+from qiskit import QuantumCircuit
+import numpy as np
+from qiskit_aer import AerSimulator
+from qiskit_aer.noise import NoiseModel, depolarizing_error, ReadoutError
+from qiskit.visualization import plot_histogram 
+import typing
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+
+from qiskit_aer import Aer
+from qiskit import transpile
+from qiskit_aer.primitives import Sampler
+from qiskit.primitives.backend_sampler import BackendSampler
+from qiskit.quantum_info import Statevector
+from qiskit.circuit import Instruction
+from matplotlib import pyplot as plt
+from qiskit.quantum_info import state_fidelity
+
+class SteaneBlock(QuantumCircuit):
+    def __init__(self, q_num, c_num, is_ideal):
+        qreg = QuantumRegister(q_num, name='q')
+        creg = ClassicalRegister(c_num, name='c')
+        super().__init__(qreg, creg)
+        self.is_ideal = is_ideal
+    
+
+class SteaneEnc(SteaneBlock):
+    def __init__(steane_enc_block, amp_0, amp_1, q_num, is_ideal, is_counts):
+
+        if (q_num != 7)  and  (q_num != 8):
+            raise ValueError("SteaneEnc Qubit Number must be 7 or 8")
+        
+        if is_ideal == True:
+            steane_enc_block = QuantumCircuit(q_num, q_num, name="steane_block")
+        else:
+            steane_enc_block = QuantumCircuit(q_num, name="steane_block_noise")
+
+        if q_num == 8:
+            
+            steane_enc_block.h(0)
+            steane_enc_block.h(4)
+            steane_enc_block.h(6)
+
+            steane_enc_block.initialize([amp_0, amp_1], 6)
+
+            steane_enc_block.barrier()
+
+            steane_enc_block.cx(0, 1)
+            steane_enc_block.cx(4, 5)
+            steane_enc_block.cx(6, 3)
+
+            steane_enc_block.cx(4,2)
+            steane_enc_block.cx(6,5)
+
+            steane_enc_block.cx(0,3)
+            steane_enc_block.cx(4,1)
+
+            steane_enc_block.cx(3,2)
+
+            steane_enc_block.cx(1,7)
+            steane_enc_block.cx(3,7)
+            steane_enc_block.cx(5,7)
+
+            steane_enc_block.barrier()
+
+            for i in range(0, 8):
+                steane_enc_block.h(i)
+            steane_enc_block.barrier()
+
+        elif q_num == 7:
+
+            for i in range(1,4):
+                steane_enc_block.h(i)
+            
+            steane_enc_block.h(6)
+            steane_enc_block.initialize([amp_0, amp_1], 6)
+
+            steane_enc_block.barrier()
+
+            steane_enc_block.cx(1,0)
+            steane_enc_block.cx(2,4)
+            steane_enc_block.cx(6,5)
+
+            steane_enc_block.cx(2,0)
+            steane_enc_block.cx(3,5)
+            steane_enc_block.cx(6,4)
+            steane_enc_block.cx(2,6)
+            steane_enc_block.cx(3,4)
+            steane_enc_block.cx(1,5)
+            steane_enc_block.cx(1,6)
+            steane_enc_block.cx(3,0)
+            
+            steane_enc_block.barrier()
+
+            # switch to measuring in other basis
+            for i in range(0, 7):
+                steane_enc_block.h(i)
+            
+        return steane_enc_block
+    
+    def convert_SteaneEnc_instr(steane_enc_block):
+        steane_to_instr = steane_enc_block.to_instruction()
+        return steane_to_instr
+    
+    def prepare_state(steane_enc_block, qc_prep: QuantumCircuit,  log_block: Instruction, num: int, is_ideal: bool):
+        
+        encode_block = SteaneEnc.convert_SteaneEnc_instr(steane_enc_block)
+
+        if (is_ideal == 1):
+            qc_prep.append(create_id_block(num, is_ideal), range(0,num), range(0,num))
+            qc_prep.append(encode_block, range(0,num), range(0,num))
+            qc_prep.append(create_id_block(num, is_ideal), range(0,num), range(0,num))
+            qc_prep.append(log_block,range(0,num), range(0,num))
+            qc_prep.draw('mpl')
+        else:
+            qc_prep.append(create_id_block(num, is_ideal), range(0,num))
+            qc_prep.append(encode_block, range(0,num))
+            qc_prep.append(create_id_block(num, is_ideal), range(0,num))
+            qc_prep.append(log_block,range(0,num))
+            qc_prep.draw('mpl')
+
+    def preparation_results(steane_enc_block, qc_arr: list, q_num: int,  is_ideal: bool, is_counts:bool,  noise_model: NoiseModel, prep_num = 3):
+
+
+        qca = QuantumCircuit(q_num, q_num)
+        qcb = QuantumCircuit(q_num, q_num)
+        qcc = QuantumCircuit(q_num, q_num)
+ 
+
+
+
+        qc_arr = [qca, qcb, qcc]
+
+
+
+        state_arr = []
+        counts_arr = []
+
+        
+
+        
+        for i in range(0,prep_num): 
+
+            SteaneEnc.prepare_state(steane_enc_block, qc_arr[i], create_id_block(q_num, is_ideal), q_num, is_ideal)
+
+            if (is_counts == True):
+                for j in range(0,7):
+                    qc_arr[i].measure(j,j)
+
+
+            if is_counts == False:
+                if is_ideal == True:
+                    simulator = Aer.get_backend('statevector_simulator')
+                    steane_t = transpile(qc_arr[i], simulator)
+                    job = simulator.run(steane_t, shots = 1000)
+                    result = job.result()
+                    state = result.get_statevector()
+                    state_arr.append(state)
+                else:
+                    qc_arr[i].save_statevector(label='state_post', pershot=True, conditional=True)
+                    backend = AerSimulator(noise_model=noise_model)
+                    transpiled = transpile(qc_arr[i], backend)
+                    job = backend.run(transpiled, shots=1000)
+                    result = job.result()
+                    state = result.data()['state_post']
+                    for shot in state['0x0']:
+                        state_arr.append(shot.data)
+            else:
+                backend = AerSimulator(noise_model=noise_model)
+                transpiled = transpile(qc_arr[i], backend)
+                job = backend.run(transpiled, shots = 1000)
+                result = job.result()
+                counts_n = result.get_counts()
+                counts_arr.append(counts_n)
+
+        return [qc_arr, state_arr, counts_arr]
+    
+class SteaneDec(SteaneBlock):
+    def __init__(steane_dec_block, q_num, c_num, is_ideal):
+        super().__init__(q_num, c_num, is_ideal)
+
+    def steane_dec(steane_dec_block, q_num):
+
+        if (q_num != 7)  or  (q_num != 8):
+            raise ValueError("SteaneDec Qubit Number must be 7 or 8")
+    
+        if q_num == 8:
+            for i in range(0, 8):
+                steane_dec_block.h(i)
+            
+            steane_dec_block.barrier()
+
+            steane_dec_block.cx(5,7)
+            steane_dec_block.cx(3,7)
+            steane_dec_block.cx(1,7)
+
+            steane_dec_block.cx(3,2)
+
+            steane_dec_block.cx(4,1)
+            steane_dec_block.cx(0,3)
+
+            steane_dec_block.cx(6,5)
+            steane_dec_block.cx(4,2)
+
+            steane_dec_block.cx(6, 3)
+            steane_dec_block.cx(4, 5)
+            steane_dec_block.cx(0, 1)
+
+            steane_dec_block.barrier()
+
+            return steane_dec_block
+        elif q_num == 7:
+            
+            for i in range(0, 7):
+                steane_dec_block.h(i)
+
+            steane_dec_block.barrier()
+
+            steane_dec_block.cx(3,0)
+            steane_dec_block.cx(1,6)
+            steane_dec_block.cx(1,5)
+            steane_dec_block.cx(3,4)
+            steane_dec_block.cx(2,6)
+            steane_dec_block.cx(6,4)
+            steane_dec_block.cx(3,5)
+            steane_dec_block.cx(2,0)
+
+            steane_dec_block.cx(6,5)
+            steane_dec_block.cx(2,4)
+            steane_dec_block.cx(1,0)
+
+
+            steane_dec_block.barrier()
+
+
+            steane_dec_block.measure(6,0)
+
+
+            return steane_dec_block
+        
+
+
+
+
+
+### Helper functions
+
+def create_h_block(num: int, is_ideal: bool):
+    if (is_ideal == 1):
+        h_block = QuantumCircuit(num, num, name="h_block")
+    else:
+        h_block = QuantumCircuit(num, name="h_block_noise")
+
+    for i in range(0, num):
+        h_block.h(i)
+
+    # convert to instruction
+    h_block_instr = h_block.to_instruction()
+    return h_block_instr
+        
+
+# applies cz to n qubits
+def create_cz_block(num: int, is_ideal: bool):
+    if (is_ideal == 1):
+        cz_block = QuantumCircuit(num*2, num*2, name="cz_block")
+    else:
+        cz_block = QuantumCircuit(num*2, name="cz_block_noise")
+    
+    for i in range(0, num):
+        cz_block.cz(i, i + num)
+        print(num)
+
+    # convert to instruction
+    cz_block_instr = cz_block.to_instruction()
+    return cz_block_instr
+    
+# applies identity to 7 qubits
+def create_id_block(num: int, is_ideal: bool):
+
+    if (is_ideal == 1):
+        id_block = QuantumCircuit(num, num, name = "id_block")
+    else:
+        id_block = QuantumCircuit(num, name = "id_block")
+    
+
+    for i in range(0, num):
+        id_block.id(i)
+
+    id_block_instr = id_block.to_instruction()
+    return id_block_instr
+                    
+
+                 # applies x to 7 qubits
+def create_x_block(num: int, is_ideal: bool):
+    if (is_ideal == 1):
+        x_block = QuantumCircuit(num, num, name = "x_block")
+    else:
+        x_block = QuantumCircuit(num, name = "x_block")
+    
+
+    for i in range(0, num):
+        x_block.x(i)
+
+    x_block_instr = x_block.to_instruction()
+    return x_block_instr       
--- gate_synthesis/helpers.py
+++ gate_synthesis/helpers.py
@@ -6,8 +6,10 @@ import copy
 import pickle
 from qiskit.quantum_info import random_unitary
 
+# Exotic magic-state angle
 theta = np.arctan(np.sqrt((np.sqrt(5) - 1) / 2))
 
+# Gate-name to integer-id mapping
 gate_to_id = {
     'X': 0,
     'Y': 1,
@@ -21,6 +23,7 @@ gate_to_id = {
     'I': 9
 }
 
+# Integer-id to gate-name mapping
 id_to_gate = {
     0: 'X',
     1: 'Y',
@@ -36,30 +39,42 @@ id_to_gate = {
 
 
 def cost_function1(n: int) -> int:
+    # Magic-gate cost model
     return (4 - 3*2**(3-n))
 
+
 def rz_gate(theta: float):
+    # Single-qubit Z rotation
     return np.array([[np.exp(-1j * theta/2), 0], [0, np.exp(1j * theta/2)]])
 
-# Creating the Tau gate sets
+
+# Return allowed odd k values for tau rotations
 def odd_k(denom: int) -> List[int]:
-    k_list = []
     highest = denom//2 - 1
-    k_list = list(range(-highest, highest+1, 2))
-    return k_list
-    
+    return list(range(-highest, highest+1, 2))
+
+
 def tau_set(n: int):
+    # Build tau gate set for a given denominator
     tau = []
     denom = 2**(n-1)
     k_list = odd_k(denom)
     cost = cost_function1(n)
+
     for k in k_list:
         gate = rz_gate(k*np.pi/denom)
-        tau.append({"gate": gate, "t_count": cost, "id": gate_to_id[f'Rz({k}*π/{denom})'], "magic_gate": True})
+        tau.append({
+            "gate": gate,
+            "t_count": cost,
+            "id": gate_to_id[f'Rz({k}*π/{denom})'],
+            "magic_gate": True
+        })
+
     return tau
 
+
 def key_from_U(U, tol: float = 1e-12, digits: int = 12):
-    # U = aI + i(xX + yY + zZ)
+    # Convert U into a canonical SU(2) key: U = aI + i(xX + yY + zZ)
     det = np.linalg.det(U)
     U = U / np.sqrt(det + 0j)
 
@@ -72,30 +87,38 @@ def key_from_U(U, tol: float = 1e-12, digits: int = 12):
         (0.5 * (a - d)).imag
     ], dtype=float)
 
+    # Fix global sign ambiguity
     for val in key:
         if abs(val) > tol:
             if val < 0:
                 key = -key
             break
 
+    # Remove numerical noise
     key[np.abs(key) < tol] = 0.0
     key = np.round(key, digits)
 
     return tuple(key)
-        
+
+
 def rebuild_U(key):
-    # key = (a,x,y,z)
-    a,x,y,z = key
+    # Rebuild unitary from key = (a, x, y, z)
+    a, x, y, z = key
+
     identity = np.eye(2, dtype='complex')
     pauli_x = np.array([[0, 1], [1, 0]])
     pauli_y = np.array([[0, -1j], [1j, 0]])
     pauli_z = np.array([[1, 0], [0, -1]])
-    
+
     return a*identity + 1j*(x*pauli_x + y*pauli_y + z*pauli_z)
-    
+
+
+##################################################################################################################
 # Database structure
+
 class SequenceDB:
     def __init__(self):
+        # Store each unique unitary and how it was generated
         self.index_of = {}      # key -> integer index
         self.keys = []          # index -> key tuple
         self.parent = []        # index -> parent index, -1 for root
@@ -112,6 +135,7 @@ class SequenceDB:
         return self.index_of.get(key)
 
     def add_root(self, key):
+        # Add identity/root node
         if key in self.index_of:
             raise ValueError(f"Duplicate root key: {key}")
 
@@ -121,9 +145,11 @@ class SequenceDB:
         self.parent.append(-1)
         self.last_gate.append(-1)
         self.magic_count.append(0)
+
         return idx
 
     def add(self, key, parent_idx, gate_id, magic_count):
+        # Add new sequence if it is not already in the database
         if key in self.index_of:
             return self.index_of[key], False
 
@@ -133,36 +159,44 @@ class SequenceDB:
         self.parent.append(int(parent_idx))
         self.last_gate.append(int(gate_id))
         self.magic_count.append(int(magic_count))
+
         return idx, True
 
     def reconstruct_seq_ids(self, idx):
+        # Trace backward from node to root
         seq = []
+
         while idx != -1:
             g = self.last_gate[idx]
             if g != -1:
                 seq.append(g)
             idx = self.parent[idx]
+
         seq.reverse()
         return seq
 
     def reconstruct_seq_names(self, idx, id_to_gate):
+        # Convert gate IDs into readable gate names
         return [id_to_gate[g] for g in self.reconstruct_seq_ids(idx)]
 
     def get_magic_count(self, idx):
         return self.magic_count[idx]
 
     def save(self, filepath):
+        # Save compressed database state
         state = {
             "keys": self.keys,
             "parent": np.asarray(self.parent, dtype=np.int32),
             "last_gate": np.asarray(self.last_gate, dtype=np.int16),
             "magic_count": np.asarray(self.magic_count, dtype=np.int16),
         }
+
         with open(filepath, "wb", buffering=16 * 1024 * 1024) as f:
             pickle.dump(state, f, protocol=pickle.HIGHEST_PROTOCOL)
 
     @classmethod
     def load(cls, filepath):
+        # Load database and rebuild key lookup
         with open(filepath, "rb") as f:
             state = pickle.load(f)
 
@@ -172,13 +206,15 @@ class SequenceDB:
         db.last_gate = state["last_gate"].tolist()
         db.magic_count = state["magic_count"].tolist()
         db.index_of = {k: i for i, k in enumerate(db.keys)}
+
         return db
-    
-    
+
+
 ##################################################################################################################
 # Algorithm
 
 def reconstruct_unitary_from_index(db, idx, gate_matrix):
+    # Reconstruct unitary corresponding to database index
     U = np.eye(2, dtype=complex)
     gate_ids = []
 
@@ -190,18 +226,23 @@ def reconstruct_unitary_from_index(db, idx, gate_matrix):
         U = gate_matrix[gid] @ U
 
     return U
+
+
 def generate_sequences(base_gate_set, max_magic_count, previous=None):
+    # Generate all reachable sequences up to a magic-count cutoff
     clifford_gates = [g for g in base_gate_set if not g["magic_gate"]]
     magic_gates = [g for g in base_gate_set if g["magic_gate"]]
+
     gate_matrix = {g["id"]: np.asarray(g["gate"], dtype=complex) for g in base_gate_set}
     magic_ids = {g["id"] for g in magic_gates}
 
     if previous is None:
         db = SequenceDB()
 
-        # root = identity
+        # Start from identity
         root_idx = db.add_root((1.0, 0.0, 0.0, 0.0))
 
+        # Add all single-gate sequences
         for g in clifford_gates + magic_gates:
             U = gate_matrix[g["id"]]
             key = key_from_U(U)
@@ -217,6 +258,7 @@ def generate_sequences(base_gate_set, max_magic_count, previous=None):
         start_idx = 1  # skip root
 
     else:
+        # Continue from existing database
         db = copy.deepcopy(previous)
 
         if max(db.magic_count, default=0) >= max_magic_count:
@@ -232,6 +274,7 @@ def generate_sequences(base_gate_set, max_magic_count, previous=None):
         current_magic_count = db.magic_count[i]
         last_gate_id = db.last_gate[i]
 
+        # Alternate Clifford and magic layers
         last_was_clifford = (last_gate_id == -1) or (last_gate_id not in magic_ids)
         candidates = magic_gates if last_was_clifford else clifford_gates
 
@@ -256,9 +299,11 @@ def generate_sequences(base_gate_set, max_magic_count, previous=None):
 
     return db
 
+
 ##################################################################################################################
 # Approximating unitaries
 
+# Gate matrices used for synthesis
 gate_dict = {}
 gate_dict['I'] = np.eye(2, dtype='complex')
 gate_dict['X'] = np.array([[0, 1], [1, 0]])
@@ -269,70 +314,103 @@ gate_dict['S'] = rz_gate(np.pi/2)
 gate_dict['Sdg'] = rz_gate(-np.pi/2)
 gate_dict['Rz(-1*π/4)'] = np.array(TdgGate().to_matrix())
 gate_dict['Rz(1*π/4)'] = np.array(TGate().to_matrix())
-gate_dict['EMS'] = np.array([[np.cos(theta/2), -np.sin(theta/2)], [np.sin(theta/2), np.cos(theta/2)]])
+gate_dict['EMS'] = np.array([
+    [np.cos(theta/2), -np.sin(theta/2)],
+    [np.sin(theta/2), np.cos(theta/2)]
+])
+
 
 def distance(U, V):
-    # U and V both have the form (a, x, y, z)
+    # Distance between two SU(2) key vectors
     if np.allclose(U, V):
         return 0
     else:
-        return np.sqrt( (1 - abs(np.dot(U, V))) + 0j).real
-    
+        return np.sqrt((1 - abs(np.dot(U, V))) + 0j).real
+
+
 def db_to_nn(db):
+    # Build nearest-neighbor index from database keys
     keys = np.asarray(db.keys, dtype=float)
 
     nn = NearestNeighbors(algorithm="auto", metric="euclidean")
     nn.fit(keys)
+
     return nn
 
+
 def unitary_from_key(db, key, gate_matrix):
+    # Recover unitary and gate sequence from key
     idx = db.get_index(key)
+
     if idx is None:
         raise KeyError(f"Key not found in database: {key}")
 
-    # Collect gate ids from this node back to the root
+    # Collect gate ids from target back to root
     gate_ids = []
+
     while idx != -1:
         gid = db.last_gate[idx]
         if gid != -1:
             gate_ids.append(gid)
         idx = db.parent[idx]
 
-    # Reverse so they go root -> ... -> target
+    # Apply gates in forward order
     gate_ids.reverse()
 
-    # Rebuild the unitary
     U = np.eye(2, dtype=complex)
+
     for gid in gate_ids:
         U = gate_matrix[gid] @ U
 
     return U, gate_ids
 
+
 def closest_neighbors(unitary, db, nn, epsilon, base_gate_set):
+    # Search for database unitaries within epsilon of target unitary
     r = np.sqrt(2) * epsilon
-    idxs = nn.radius_neighbors(np.array(key_from_U(unitary), dtype=float).reshape(1, -1), radius=r, return_distance=False)
-    
+    target_key = np.array(key_from_U(unitary), dtype=float).reshape(1, -1)
+
+    idxs = nn.radius_neighbors(
+        target_key,
+        radius=r,
+        return_distance=False
+    )
+
     if len(idxs[0]) == 0:
         print(f"No closest neighbors given epsilon {epsilon}.")
         return None, None, None, None
-    
-    gate_matrix = {g["id"]: np.asarray(g["gate"], dtype=complex) for g in base_gate_set}
-    
+
+    gate_matrix = {
+        g["id"]: np.asarray(g["gate"], dtype=complex)
+        for g in base_gate_set
+    }
+
     best_magic = None
     best_distance = 1
     best_unitary = None
     best_seqs = None
-    
+
+    # Choose lowest magic count, then smallest distance
     for i in idxs[0]:
-        temp_distance = distance(np.array(db.keys[i], dtype=float), np.array(key_from_U(unitary), dtype=float))
-        
+        temp_distance = distance(
+            np.array(db.keys[i], dtype=float),
+            np.array(key_from_U(unitary), dtype=float)
+        )
+
         if temp_distance <= epsilon:
             temp_magic = db.magic_count[i]
-            if best_magic is None or temp_magic < best_magic or (temp_magic == best_magic and temp_distance < best_distance):
+
+            if (
+                best_magic is None
+                or temp_magic < best_magic
+                or (temp_magic == best_magic and temp_distance < best_distance)
+            ):
                 best_magic = temp_magic
                 best_distance = temp_distance
-                best_unitary, best_seqs = unitary_from_key(db, db.keys[i], gate_matrix)
-    
-    return best_magic, best_distance, best_unitary, best_seqs
-    
+                best_unitary, best_seqs = unitary_from_key(
+                    db,
+                    db.keys[i],
+                    gate_matrix
+                )
 
+    return best_magic, best_distance, best_unitary, best_seqs
